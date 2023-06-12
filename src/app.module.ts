275a60f6aea077fb1257e51064796cd3c86c6076
mickelsengerald <mickelsengerald@gmail.com>
2023-06-12 10:59:53 -0400

    modularizando auth admin experience jobs languages reviews users subjects
     generate module nombre  generate controller nombre --no-spec  generate service nombre --no-spec

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { AdminModule } from './admin/admin.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { LanguagesModule } from './languages/languages.module';
+import { ExperienceModule } from './experience/experience.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { SubjectsModule } from './subjects/subjects.module';
+import { JobsModule } from './jobs/jobs.module';
 
 
 @Module({
-  imports: [],
+  imports: [AdminModule, UsersModule, AuthModule, LanguagesModule, ExperienceModule, ReviewsModule, SubjectsModule, JobsModule],
   
 })
 export class AppModule {}
