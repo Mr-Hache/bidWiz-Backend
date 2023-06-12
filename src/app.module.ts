bd3ba21f7fc21bb642dfe3f2c396caae31e0ed96
mickelsengerald <mickelsengerald@gmail.com>
2023-06-12 11:03:24 -0400

    conexion con mongodb
    import del module de mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,11 @@ import { ExperienceModule } from './experience/experience.module';
 import { ReviewsModule } from './reviews/reviews.module';
 import { SubjectsModule } from './subjects/subjects.module';
 import { JobsModule } from './jobs/jobs.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
-  imports: [AdminModule, UsersModule, AuthModule, LanguagesModule, ExperienceModule, ReviewsModule, SubjectsModule, JobsModule],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/bidwizdb'), AdminModule, UsersModule, AuthModule, LanguagesModule, ExperienceModule, ReviewsModule, SubjectsModule, JobsModule],
   
 })
 export class AppModule {}
