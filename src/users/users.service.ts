0d9bc9dbc586ae308a45af11a47f63ed4ee43efc
mickelsengerald <mickelsengerald@gmail.com>
2023-06-13 17:52:32 -0400

    delete por sidabled funcionando

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, ConflictException, InternalServerErrorException, BadRequestException } from '@nestjs/common';
+import { Injectable, ConflictException, InternalServerErrorException, BadRequestException, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from 'src/schemas/user.schema';
 import { Model } from 'mongoose';
@@ -34,23 +34,39 @@ export class UsersService {
     }
 
     async findAllAdmin(): Promise<User[]> {
-        return this.userModel.find({ isDisabled: false, role: { $ne: 'admin' }}).exec();
+        return this.userModel.find({role: { $ne: 'admin' }}).exec();
     }
     
     async findOne(username: string): Promise<User> {
         const user = await this.userModel.findOne({ username: username, isDisabled: false }).exec();
+        if (!user) {
+            throw new NotFoundException(`User with username ${username} not found`);
+        }
         return user;
     }
     
     async updatePassword(username: string, updateUserPasswordDto: UpdateUserPasswordDto): Promise<User> {
-        return this.userModel.findOneAndUpdate({ username: username, isDisabled: false }, { password: updateUserPasswordDto.newPassword }).exec();
+        const user = await this.userModel.findOneAndUpdate({ username: username, isDisabled: false }, { password: updateUserPasswordDto.newPassword }).exec();
+        if (!user) {
+            throw new NotFoundException(`User with username ${username} not found`);
+        }
+        return user;
     }
     
     async updateWizard(username: string, updateUserWizardDto: UpdateUserWizardDto): Promise<User> {
-        return this.userModel.findOneAndUpdate({ username: username, isDisabled: false }, updateUserWizardDto).exec();
+        const user = await this.userModel.findOneAndUpdate({ username: username, isDisabled: false }, updateUserWizardDto).exec();
+        if (!user) {
+            throw new NotFoundException(`User with username ${username} not found`);
+        }
+        return user;
     }
     
     async disable(username: string): Promise<User> {
-        return this.userModel.findOneAndUpdate({ username: username }, { isDisabled: true }).exec();
+        const user = await this.userModel.findOneAndUpdate({ username: username }, { isDisabled: true }).exec();
+        if (!user) {
+            throw new NotFoundException(`User with username ${username} not found`);
+        }
+        return user;
     }
+    
 }
