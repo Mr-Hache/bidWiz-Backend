bf62072d45198bd03960211fec7d643aa5abe646
mickelsengerald <mickelsengerald@gmail.com>
2023-06-13 14:33:26 -0400

    main del back

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    console.log('Creating Nest application...');
+    const app = await NestFactory.create(AppModule);
+    console.log('Nest application created.');
+
+    app.enableCors()
+    app.useGlobalPipes(new ValidationPipe())
+
+    console.log('Starting server...');
+    await app.listen(3000);
+    console.log('Server started. Application is running on: http://localhost:3000');
+  } catch (error) {
+    console.error('Error while starting the application', error);
+  }
 }
+
+
 bootstrap();
