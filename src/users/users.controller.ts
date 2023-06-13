d88299164824d0bc4c8f4dc325fa88f7faab4949
mickelsengerald <mickelsengerald@gmail.com>
2023-06-13 12:47:15 -0400

    actualizacion user controller mas service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Delete } from '@nestjs/common';
+import { CreateUserDto } from 'src/dto/create-user.dto';
+import { UpdateUserPasswordDto } from 'src/dto/update-user-password.dto';
+import { UpdateUserWizardDto } from 'src/dto/update-user-wizard.dto';
+import { User } from 'src/schemas/user.schema';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':username')
+  async findOne(@Param('username') username: string): Promise<User> {
+    return this.usersService.findOne(username);
+  }
+
+  @Patch(':username/password')
+  async updatePassword(
+    @Param('username') username: string,
+    @Body() updateUserPasswordDto: UpdateUserPasswordDto
+  ): Promise<User> {
+    return this.usersService.updatePassword(username, updateUserPasswordDto);
+  }
+
+  @Patch(':username/wizard')
+  async updateWizard(
+    @Param('username') username: string,
+    @Body() updateUserWizardDto: UpdateUserWizardDto
+  ): Promise<User> {
+    return this.usersService.updateWizard(username, updateUserWizardDto);
+  }
+
+  @Delete(':username')
+  async disable(@Param('username') username: string): Promise<User> {
+    return this.usersService.disable(username);
+  }
+}
