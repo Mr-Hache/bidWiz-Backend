75f4afebca406273e43a43e0a55640b08b393023
mickelsengerald <mickelsengerald@gmail.com>
2023-06-21 14:33:06 -0400

    job controller para el create job

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateJobDto } from 'src/dto/create-job.dto';
+import { JobsService } from './jobs.service';
 
 @Controller('jobs')
-export class JobsController {}
+export class JobsController {
+    constructor(private readonly jobsService: JobsService) {}
+
+  @Post()
+  async createJob(@Body() createJobDto: CreateJobDto) {
+    const createdJob = await this.jobsService.createJob(createJobDto);
+    return createdJob;
+  }
+}
