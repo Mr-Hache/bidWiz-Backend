443afacc8ddaed5f4a13c6d1592c25c301309632
mickelsengerald <mickelsengerald@gmail.com>
2023-06-21 16:35:16 -0400

    comente todo  job para railway

--- src/dto/update-job-worker.dto.ts
+++ src/dto/update-job-worker.dto.ts
@@ -1,9 +1,18 @@
 import { IsNotEmpty, IsEnum, IsString } from 'class-validator';
 import { JobStatus } from 'src/schemas/jobStatus.enum';
+import { Types } from 'mongoose';
 
 export class UpdateJobWorkerDto{
     @IsNotEmpty()
     @IsEnum(JobStatus)
     status: JobStatus;
+
+    @IsNotEmpty()
+    @IsString()
+    jobId: string;
+
+    @IsNotEmpty()
+    @IsString()
+    workerId: string;
 
 }
