a95dc6fcd024bbee894277631bdd1c0d10212ad7
mickelsengerald <mickelsengerald@gmail.com>
2023-06-21 14:37:05 -0400

    modifique el job module para conectar controller con service

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JobsController } from './jobs.controller';
 import { JobsService } from './jobs.service';
+import { Job, JobSchema } from 'src/schemas/jobs.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Job.name, schema: JobSchema }])],
   controllers: [JobsController],
   providers: [JobsService]
 })
