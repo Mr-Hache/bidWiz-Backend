bee1ebc7a80039e4e9512140ebf97a5f283f484e
mickelsengerald <mickelsengerald@gmail.com>
2023-06-21 14:42:09 -0400

    arregle el job module para agregar el user y userschema

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -3,9 +3,10 @@ import { JobsController } from './jobs.controller';
 import { JobsService } from './jobs.service';
 import { Job, JobSchema } from 'src/schemas/jobs.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/schemas/user.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Job.name, schema: JobSchema }])],
+  imports: [MongooseModule.forFeature([{ name: Job.name, schema: JobSchema }, { name: User.name, schema: UserSchema }])],
   controllers: [JobsController],
   providers: [JobsService]
 })
