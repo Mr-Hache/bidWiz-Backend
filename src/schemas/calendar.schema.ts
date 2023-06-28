649f218b5741cb01fa14a497ae773238910027c1
mickelsengerald <mickelsengerald@gmail.com>
2023-06-28 11:07:49 -0400

    agregue el esquema de calendar

---
+++ src/schemas/calendar.schema.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type CalendarDocument = Calendar & Document;
+
+@Schema()
+export class TimeSlot {
+  @Prop({ default: false })
+  isBooked: boolean;
+}
+
+export const TimeSlotSchema = SchemaFactory.createForClass(TimeSlot);
+
+@Schema()
+export class Day {
+  @Prop({ type: Map, of: TimeSlotSchema })
+  timeSlots: Map<string, TimeSlot>;
+}
+
+export const DaySchema = SchemaFactory.createForClass(Day);
+
+@Schema()
+export class Calendar {
+  @Prop({ type: Map, of: DaySchema })
+  days: Map<string, Day>;
+}
+
+export const CalendarSchema = SchemaFactory.createForClass(Calendar);
