29fb4c9eb5c39b5c355d08dd59b53bd1608a3608
mickelsengerald <mickelsengerald@gmail.com>
2023-07-04 17:15:43 -0400

    defaukt del about lorem ipsum

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -49,7 +49,7 @@ export class User {
   @Prop({ default: 0})
   pricePerThree: number;
 
-  @Prop({ default: ""})
+  @Prop({ default: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."})
   aboutMe: string;
 
   @Prop({ default: false })
